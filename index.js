82670c78d1db40c0f314060d6e7b5cc751795fe5
sumitramanga <sumitramanga@gmail.com>
2018-12-12 09:55:10 +1300

    show key

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.end('Welcome to the shopping api');
+    res.end(process.env.behanceKey);
 })
 
 app.set('port', (process.env.PORT || 3000));
