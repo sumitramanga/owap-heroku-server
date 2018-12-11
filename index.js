9d5358c4d7d002436459f7de7465e58d57dd3d20
sumitramanga <sumitramanga@gmail.com>
2018-12-12 09:59:40 +1300

    Added required and module set up

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const request = require('request');
+const Behance = require('node-behance-api');
 
 var app = express();
 
+// Allow cors
+app.use(cors());
+
+// Behance module setup
+const behance = new Behance({'client_id': `${process.env.behanceKey}`});
+Behance.initOptions();
+
 app.get('/', function(req, res){
-    res.end(process.env.behanceKey);
+    res.end('Welcome to the api');
 })
 
 app.set('port', (process.env.PORT || 3000));
